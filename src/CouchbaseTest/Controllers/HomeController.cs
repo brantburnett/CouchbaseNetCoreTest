462912f0ea015263e1ed9542e31eaa8108e0f52b
brantburnett <bburnett@centeredgesoftware.com>
2016-09-05 22:08:09 -0400

    Initial commit

---
+++ src/CouchbaseTest/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Couchbase;
+using CouchbaseTest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouchbaseTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var bucket = ClusterHelper.GetBucket("travel-sample");
+
+            var result = bucket.Query<Airline>(
+                "SELECT airline.* FROM `travel-sample` airline WHERE type = 'airline' ORDER BY airline.name LIMIT 20");
+
+            if (!result.Success)
+            {
+                throw new Exception("Error: " + result.Message);
+            }
+            else
+            {
+                return View(result.Rows);
+            }
+        }
+
+        public async Task<IActionResult> Airline(int id)
+        {
+            var bucket = ClusterHelper.GetBucket("travel-sample");
+
+            var result = await bucket.GetDocumentAsync<Airline>("airline_" + id);
+
+            if (!result.Success)
+            {
+                throw new Exception("Error: " + result.Message);
+            }
+            else
+            {
+                return View(result.Document.Content);
+            }
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/CouchbaseTest/Models/Airline.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CouchbaseTest.Models
+{
+    public class Airline
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Callsign { get; set; }
+        public string Icao { get; set; }
+    }
+}
